d99d57d034ca969c573b264e109b92c5ffb9327d
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-08-11 22:28:49 -0500

    Implement custom User model with locality field and update related serializers and views

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,10 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
+from localities.models import Locality
+
+User = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -9,11 +13,12 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         write_only=True, required=True, validators=[validate_password]
     )
     password2 = serializers.CharField(write_only=True, required=True)
+    locality_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
 
     class Meta:
         model = User
         fields = (
-            "username", "password", "password2", "email", "first_name", "last_name", "role"
+            "username", "password", "password2", "email", "first_name", "last_name", "role", "locality_id"
         )
         extra_kwargs = {
             "email": {"required": True}
@@ -24,7 +29,24 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 {"password": "Password fields didn't match."}
             )
+        
+        role = attrs.get("role")
+        locality_id = attrs.get("locality_id")
+
+        if role == "user" and not locality_id:
+            raise serializers.ValidationError(
+                {"locality_id": "Locality is required for users with role 'user'."}
+            )
+
         return attrs
+    
+    def validate_locality_id(self, value):
+        """
+        Valida que la localidad enviada exista en la base de datos.
+        """
+        if value and not Locality.objects.filter(id=value).exists():
+            raise serializers.ValidationError(f"Locality with id '{value}' does not exist.")
+        return value
 
     def validate_role(self, value):
         """
@@ -37,9 +59,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         role_name = validated_data.pop("role")
         validated_data.pop("password2")
+        locality_id = validated_data.pop("locality_id", None)
 
         user = User.objects.create_user(
-            **validated_data
+            **validated_data,
+            locality_id=locality_id
         )
 
         group = Group.objects.get(name=role_name)
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .serializers import UserRegistrationSerializer
 
+User = get_user_model()
 
 class UserRegistrationView(generics.CreateAPIView):
     """
