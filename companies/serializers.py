2e09425b0b0b7b7d43e795ced910510227208599
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-08-17 23:57:03 -0500

    Add waste category fields to CompanySerializer for enhanced API integration

--- companies/serializers.py
+++ companies/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Company
+from waste_categories.models import WasteCategory
+from waste_categories.serializers import WasteCategorySerializer
 
 class CompanySerializer(serializers.ModelSerializer):
+    waste_categories = WasteCategorySerializer(many=True, read_only=True)
+    waste_category_ids = serializers.PrimaryKeyRelatedField(
+        queryset=WasteCategory.objects.all(),
+        many=True,
+        write_only=True,
+        source='waste_categories'
+    )
+
     class Meta:
         model = Company
-        fields = '__all__'
+        fields = ('id', 'name', 'waste_categories', 'waste_category_ids')
