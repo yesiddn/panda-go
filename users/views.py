cd7c7662d6e7d883e7a71d018084552322346481
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-08-18 19:31:49 -0500

    Assign available route to request upon user registration

--- users/views.py
+++ users/views.py
@@ -8,6 +8,8 @@ from datetime import timedelta
 from django.utils import timezone
 from collection_requests.models import Request
 from waste_categories.models import WasteCategory
+from routes.models import CollectionRoute
+from django.db.models import Count, F
 
 User = get_user_model()
 
@@ -32,13 +34,31 @@ class UserRegistrationView(generics.CreateAPIView):
                 collection_date = timezone.now().date() + timedelta(days=7)
                 address_snapshot = {'address': user.locality.name}
 
-                Request.objects.create(
+                request = Request.objects.create(
                     user=user,
                     locality=user.locality,
                     waste_category=organic_category,
                     collection_date=collection_date,
                     address_snapshot=address_snapshot,
                 )
+
+                # Find an available route
+                available_route = (
+                    CollectionRoute.objects.annotate(num_requests=Count('requests'))
+                    .filter(
+                        locality=user.locality,
+                        waste_category=organic_category,
+                        status='planned',
+                        capacity_stops__gt=F('num_requests'),
+                    )
+                    .first()
+                )
+
+                if available_route:
+                    request.route = available_route
+                    request.status = 'assigned'
+                    request.save()
+
             except WasteCategory.DoesNotExist:
                 # Handle case where 'Organico' category does not exist
                 pass
